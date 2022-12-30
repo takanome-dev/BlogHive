f52d14baeb11b0bae02df9116b6243389f2f3d20
takanome_dev <takanomedev221@gmail.com>
2022-12-30 22:49:37 +0000

    feat: add homepage

--- apps/client/src/pages/_app.tsx
+++ apps/client/src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { type AppType } from 'next/app';
 
+// TODO: remove eslint-disable-next-line and fix the issue with import/no-unresolved
+// eslint-disable-next-line import/no-unresolved
 import '@/styles/global.css';
 
 const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => (
--- apps/client/src/pages/index.tsx
+++ apps/client/src/pages/index.tsx
@@ -1,10 +1,39 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import { Button } from 'ui';
+import logoTextBlack from '../../public/logo-text-black-1.png';
 
-export default function Web() {
+export default function IndexPage() {
   return (
-    <div>
-      <h1>Web</h1>
-      <Button />
-    </div>
+    <>
+      <header className="sticky top-0 flex items-center justify-between px-16 py-2 shadow-md">
+        <Link href="/">
+          <Image src={logoTextBlack} width={200} height={200} alt="Logo" />
+        </Link>
+        <Button />
+      </header>
+      <main className="min-h-[calc(100vh-(70px))] px-16 py-8">
+        <div className="flex w-full flex-col items-center justify-center border border-red-500 py-8 text-center">
+          <h1 className="max-w-3xl text-5xl font-bold text-slate-900 ">
+            The <span className="text-blue-600">Open Source</span> Blogging
+            Platform
+          </h1>
+          <p className="text-md mt-8 max-w-2xl text-slate-500">
+            BlogHive is a platform designed specifically for writers and
+            bloggers to share their work, receive feedback, and collaborate with
+            others on their blogging journey.
+          </p>
+        </div>
+        <div className="mt-8 flex flex-col items-center">
+          <span className="rounded-lg border border-blue-600 px-2 py-1 text-blue-600">
+            Community
+          </span>
+          <p className="mt-4 max-w-lg text-xl font-semibold text-slate-700">
+            BlogHive is more than just a platform for writers and bloggers -
+            it's a <span className="text-blue-600">community</span>
+          </p>
+        </div>
+      </main>
+    </>
   );
 }
