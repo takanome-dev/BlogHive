7845cd5dc1677ba942d25f6429838646901ea68a
takanome_dev <takanomedev221@gmail.com>
2022-12-31 14:05:49 +0000

    feat: add community section

--- apps/client/src/pages/index.tsx
+++ apps/client/src/pages/index.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from 'ui';
-import logoTextBlack from '../../public/logo-text-black-1.png';
+import logoTextBlack from '@/public/logo-text-black-1.png';
+import communityImg from '@/public/community.png';
 
 export default function IndexPage() {
   return (
     <>
-      <header className="sticky top-0 flex items-center justify-between px-16 py-2 shadow-md">
+      <header className="sticky top-0 flex items-center justify-between bg-white bg-opacity-80 px-16 py-2 shadow-md backdrop-blur-md">
         <Link href="/">
           <Image src={logoTextBlack} width={200} height={200} alt="Logo" />
         </Link>
@@ -24,15 +25,33 @@ export default function IndexPage() {
             others on their blogging journey.
           </p>
         </div>
-        <div className="mt-8 flex flex-col items-center">
-          <span className="rounded-lg border border-blue-600 px-2 py-1 text-blue-600">
-            Community
-          </span>
-          <p className="mt-4 max-w-lg text-xl font-semibold text-slate-700">
-            BlogHive is more than just a platform for writers and bloggers -
-            it's a <span className="text-blue-600">community</span>
-          </p>
-        </div>
+        <section className="mt-8">
+          <div className="flex flex-col items-center">
+            <span className="rounded-lg border border-blue-600 px-2 py-1 text-blue-600">
+              Community
+            </span>
+            <p className="mt-4 max-w-lg text-xl font-semibold text-slate-700">
+              BlogHive is more than just a platform for writers and bloggers -
+              it's a <span className="text-blue-600">community</span>
+            </p>
+          </div>
+          <div className="mt-8 grid grid-cols-2 gap-12">
+            <div className="flex max-w-xl flex-col justify-center gap-8 text-slate-600">
+              <p>
+                With BlogHive, you can share your work, get feedback, and
+                collaborate with others on your blogging journey.{' '}
+              </p>
+              <p>
+                And as an open source platform, BlogHive is built and maintained
+                by a community of developers and users who are passionate about
+                helping writers and bloggers succeed.
+              </p>
+            </div>
+            <div>
+              <Image src={communityImg} alt="Community Image" />
+            </div>
+          </div>
+        </section>
       </main>
     </>
   );
