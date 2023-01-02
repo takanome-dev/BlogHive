4e5e593ba88c1eb2fbd7f2f8502bf44867edd745
takanomedev <takanomedev221@gmail.com>
2023-01-02 21:28:04 +0000

    feat: add homepage (#9)
    * feat: add homepage
    
    * feat: add community section
    
    * feat: add feattures sections
    
    * feat: add subscribe section and footer

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        slate: {
+          1: '#D9EAFB',
+          2: '#C3D6EF',
+          3: '#B6CBE3',
+          4: '#7F97B9',
+          5: '#4D678C',
+          6: '#334764',
+          7: '#253650',
+          8: '#152338',
+          9: '#0B142A',
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
