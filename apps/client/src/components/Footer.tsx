4322e85a27c885f011185abefbdee5ecb6d71f26
takanomedev <takanomedev221@gmail.com>
2023-01-04 22:47:21 +0000

    feat: Update logos (#15)
    * docs: update contributing file
    
    * docs: update contributing and readme files
    
    * feat: update logos

--- apps/client/src/components/Footer.tsx
+++ apps/client/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
-import logoTextBlack from '@/public/logo-text-black-1.png';
+import logoTextBlack from '@/public/logo-text-black.png';
 
 const Footer = () => {
   return (
--- apps/client/src/components/Header.tsx
+++ apps/client/src/components/Header.tsx
@@ -1,19 +1,17 @@
+import { Button } from 'atoms';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import { Button } from 'atoms';
 
-import logoTextBlack from '@/public/logo-text-black-1.png';
+import logoTextBlack from '@/public/logo-text-black.png';
 
-const Header = () => {
-  return (
-    <header className="sticky top-0 flex items-center justify-between bg-white bg-opacity-70 px-16 py-2 shadow-md backdrop-blur-md">
-      <Link href="/">
-        <Image src={logoTextBlack} width={200} height={200} alt="Logo" />
-      </Link>
-      <Button>Sign in</Button>
-    </header>
-  );
-};
+const Header = () => (
+  <header className="sticky top-0 flex items-center justify-between bg-white bg-opacity-70 px-16 py-2 shadow-md backdrop-blur-md">
+    <Link href="/">
+      <Image src={logoTextBlack} width={200} height={200} alt="Logo" />
+    </Link>
+    <Button>Sign in</Button>
+  </header>
+);
 
 export default Header;
